fe3344b81df14d837a7efa68d70c808ab294c521
theisen1337 <theist7901@my.uwstout.edu>
2017-11-15 17:53:14 -0600

    thomas
    back to default

--- main.cpp
+++ main.cpp
@@ -11,19 +11,8 @@ int main()
 	*/
 	//MainLoop()
 	//{
-<<<<<<< HEAD
-	<<<<<<< HEAD
-		// Insert Code Here
-		====== =
-		//...Hey matey mate
-		//blah
-		>>>>>> > 08d128d47a2f1f1a5b8851687b189166edaa0b2a
-		//}
-		int jumanji = 42;
-=======
-	//Thomas
+	//...
 	//}
->>>>>>> Project_Final(Thomas)
     return 0;
 }
 
