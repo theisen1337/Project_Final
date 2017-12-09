35f482357e8199025f7412a6493bc1a49e5aa93c
David King <kingd0559@my.uwstout.edu>
2017-12-09 16:11:15 -0600

    Sprite class

---
+++ Sprite.cpp
@@ -0,0 +1,51 @@
+#include "Sprite.h"
+
+//imagePath - location of image file
+Sprite::Sprite(const char* imagePath)
+{
+	pixels = stbi_load(path, &width, &height, &channels, STBI_rgb_alpha);
+
+	if (!pixels) {
+		throw std::runtime_error("failed to load texture image!");
+	}
+}
+
+Sprite::~Sprite()
+{
+}
+
+//returns width of sprite
+int Sprite::getWidth()
+{
+	return width;
+}
+
+//returns height of sprite
+int Sprite::getHeight()
+{
+	return height;
+}
+
+//returns number of channels in the sprite
+int Sprite::getChannels()
+{
+	return channels;
+}
+
+//returns array of pixels in the sprite
+stbi_uc* Sprite::getPixels()
+{
+	return pixels;
+}
+
+//changes the image the sprite has
+void Sprite::changeSprite(const char* imagePath)
+{
+	if (path == imagePath) return;
+
+	pixels = stbi_load(path, &width, &height, &channels, STBI_rgb_alpha);
+
+	if (!pixels) {
+		throw std::runtime_error("failed to load texture image!");
+	}
+}
