b943a6c07a718a0ee17092e76372f8215d4946c4
Alex <buchholza0654@my.uwstout.edu>
2017-12-17 14:10:02 -0600

    Updated shaders

---
+++ shaders/compile.bat
@@ -0,0 +1,3 @@
+"D:/Microsoft Visual Studio/Libraries/VulkanSDK/1.0.65.0/Bin32/glslangValidator.exe" -V shader.vert
+"D:/Microsoft Visual Studio/Libraries/VulkanSDK/1.0.65.0/Bin32/glslangValidator.exe" -V shader.frag
+pause
